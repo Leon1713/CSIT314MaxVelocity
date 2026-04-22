7774cd41233b660b44db798d84639e6ae508f69d
Leon1713 <61048222+Leon1713@users.noreply.github.com>
2026-04-22 20:44:48 +0800

    sample requirements and env for remote db in avien, make db.py connect to remote

--- db.py
+++ db.py
@@ -3,14 +3,15 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 
-load_dotenv()  # reads from .env file automatically
+loaded = load_dotenv()  # reads from .env file automatically
+
 
 DB_CONFIG = {
-    "host":       os.getenv("DB_HOST",     "localhost"),
-    "port":   int(os.getenv("DB_PORT",     "3306")),
-    "user":       os.getenv("DB_USER",     "root"),
-    "password":   os.getenv("DB_PASSWORD", "1234"),
-    "database":   os.getenv("DB_NAME",     "fundraising_db"),
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", 3306)),  # Default to 3306 if not provided
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME", "fundraising_db"),
     "autocommit": False,
 }
 
