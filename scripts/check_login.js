3c30386f9d8ee4c729076c7e203e3f7b54448a0d
Leon1713 <61048222+Leon1713@users.noreply.github.com>
2026-05-08 23:40:35 +0800

    added loader to login before session check

--- scripts/check_login.js
+++ scripts/check_login.js
@@ -9,27 +9,25 @@ async function start() {
       throw new Error(errText);
     }
     const result = await res.json().then(data => {
-      if(!data.success)
-      {
-        //document.getElementById("login-page").classList.remove("hidden");
-        window.location.href = "login.html";
+      if (!data.success) {
+        document.getElementById("login-modal-page").classList.remove("hidden");
+        document.getElementById("loader").classList.add("hidden");
         console.log("no result found");
       }
-      else if(data.success)
-      {
-        switch(data.role_id)
-        {
+      else if (data.success) {
+        switch (data.role_id) {
           case 1:
-            window.location.href ="admin_dashboard.html";
-            
+            window.location.href = "admin_dashboard.html";
+
         }
         console.log(data);
       }
     })
   }
   catch (err) {
+    document.getElementById("login-modal-page").classList.remove("hidden");
+    document.getElementById("loader").classList.add("hidden");
     console.log("Failed to connect to server. Please try again later.")
-    window.location.href = "login.html";
   }
 }
 start();
