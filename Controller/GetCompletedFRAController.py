0a95cb5c7c843dfd9c49f54891ed0122a2318557
Leon17 <yeojiayuan@gmail.com>
2026-05-16 17:43:56 +0800

    removed conn in controller

--- Controller/GetCompletedFRAController.py
+++ Controller/GetCompletedFRAController.py
@@ -3,5 +3,4 @@ from db import get_db_connection
 
 class GetCompletedFRAController:
     def getCompleted(self, fundraiser_id: int, filters: dict) -> list:
-        with get_db_connection() as conn:
-            return FundraisingActivity.getCompletedByFundraiserId(fundraiser_id, filters, conn)
+            return FundraisingActivity.getCompletedByFundraiserId(fundraiser_id, filters)
